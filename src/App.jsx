b22f448eeffa10e2e14909a6022b498e75e99bce
Sachin Raj <sachinraj6392@gmail.com>
2025-03-08 01:27:23 +0530

    css change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Styles/global.css";
-import Dashboard from "./Compoents/Dashboard";
+import "./Styles/Global.css";
+import Dashboard from "./Components/Dashboard";
 import { DashboardProvider } from "./Context/DashboardContext";
 
 const App = () => (
